0cc0630dd9b4373f5f357a895194b1c47a4b1a93
Jiahuan-Pei <j.pei@uva.nl>
2023-10-13 14:52:13 +0200

    LegoAPIWrapper by websocket

--- LEGO_dialogue_agent_openai/tools_wrappers/lego_api_wrapper.py
+++ LEGO_dialogue_agent_openai/tools_wrappers/lego_api_wrapper.py
@@ -5,41 +5,70 @@ https://github.com/kukeya/2023-SWContest/tree/main/unity-script/*.cs
 import requests
 import urllib.parse
 import inspect
+import socket
 
+"""
+When to Choose Sockets:
+- Use sockets when you need low-latency, real-time, or continuous communication.
+- For scenarios like online games, chat applications, live streaming, or any application where rapid data transfer is crucial.
+- When full-duplex communication is essential, and you want to maintain an open connection.
+
+When to Choose RESTful API:
+- Use RESTful APIs when you need a simple and web-friendly way to exchange data between applications.
+- If you want to create an HTTP-based API that other services or clients can access.
+- When real-time, low-latency communication is not a strict requirement.
+"""
 
 class LegoAPIWrapper:
     """
     all functions in AR system can be used by DA as external tools
     """
     def __init__(self):
-        self.base_url = "http://your-ar-system-api-url.com"
+        self.unity_host = "your_unity_host_ip"
+        self.unity_port = 80
+
+    def _call_unity_function_rest_api(self, function_name):
+        url = f"{self.unity_host}:{self.unity_port}/{function_name}"
+        response = requests.post(url)
+
+        if response.status_code == 200:
+            print(f"Unity function {function_name} successfully called.")
+        else:
+            print(f"Failed to call Unity function {function_name}.")
+
+    def _call_unity_function(self, function_name):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((self.unity_host, self.unity_port))
+            unity_function_name = function_name.encode("utf-8")
+            unity_response = s.sendall(unity_function_name)
+        return unity_response
 
     def callStartAssemble(self):
-        return inspect.currentframe().f_code.co_name
+        return self._call_unity_function("StartAssemble")
 
     def callNextStep(self):
-        return inspect.currentframe().f_code.co_name
+        return self._call_unity_function("NextStep")
 
     def callFrontStep(self):
-        return inspect.currentframe().f_code.co_name
+        return self._call_unity_function("FrontStep")
 
     def callExplode(self):
-        return inspect.currentframe().f_code.co_name
+        return self._call_unity_function("Explode")
 
     def callRecover(self):
-        return inspect.currentframe().f_code.co_name
+        return self._call_unity_function("Recover")
 
     def callFinishedVideo(self):
-        return inspect.currentframe().f_code.co_name
+        return self._call_unity_function("FinishedVideo")
 
     def callReShow(self):
-        return inspect.currentframe().f_code.co_name
+        return self._call_unity_function("ReShow")
 
     def callEnlarge(self):
-        return inspect.currentframe().f_code.co_name
+        return self._call_unity_function("Enlarge")
 
     def callShrink(self):
-        return inspect.currentframe().f_code.co_name
+        return self._call_unity_function("Shrink")
 
 
 if __name__ == "__main__":
